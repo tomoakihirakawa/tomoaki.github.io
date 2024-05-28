e41e4db2605a3ce63ea4534754bd8a94d8078015
平川知明 <tomoaki@hirakawatomoakinoMac-Studio.local>
2024-05-28 13:05:48 +0900

    栗田研究テーマ

---
+++ javascripts/open_links_in_new_tab.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 画像リンクに対してtarget="_blank"を追加
+    var imageLinks = document.querySelectorAll('a[href$=".png"], a[href$=".jpg"], a[href$=".jpeg"], a[href$=".gif"]');
+    imageLinks.forEach(function(link) {
+        link.setAttribute('target', '_blank');
+    });
+
+    // 外部リンクに対してtarget="_blank"を追加
+    var allLinks = document.querySelectorAll('a');
+    allLinks.forEach(function(link) {
+        if (link.hostname !== window.location.hostname) {
+            link.setAttribute('target', '_blank');
+        }
+    });
+});
